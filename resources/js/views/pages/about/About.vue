4867cced972a949ffb9879948d1958fff2ac4927
noentoro <nicholasoentoro@gmail.com>
2021-04-03 10:50:45 -0700

    init about page

--- resources/js/views/pages/about/About.vue
+++ resources/js/views/pages/about/About.vue
@@ -1,20 +1,137 @@
 <template>
   <div>
     <NavbarDefault />
+    <div class="row g-0" style="background: #2e2e2e;">
+      <div class="col-md-3 col-lg-3 order-4 order-md-2 order-lg-1 my-5 space"></div>
+      <div class="col-md-8 col-lg-8 order-2 order-lg-2 my-5">
+        <div class="container">
+          <div style="height: 120px;">
+            <h4 style="color: var(--bs-light);font-family: Poppins, sans-serif;">Nicholas Oentoro</h4>
+            <p style="color: #ffffff;">Full stack developer</p>
+            <div class="d-flex">
+              <p class="my-auto" style="color: #ffc400;">Total Experience: 1000 hours</p><button class="btn d-flex ms-auto" type="button" style="color: #ffc400;border-radius: 30px;border-style: solid;border-color: #ffc400;padding-right: 1.5em;"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" style="margin: 0em .5em;">
+              <path d="M8.68387 13.3419C8.88616 12.9381 9 12.4824 9 12C9 11.5176 8.88616 11.0619 8.68387 10.6581M8.68387 13.3419C8.19134 14.3251 7.17449 15 6 15C4.34315 15 3 13.6569 3 12C3 10.3431 4.34315 9 6 9C7.17449 9 8.19134 9.67492 8.68387 10.6581M8.68387 13.3419L15.3161 16.6581M8.68387 10.6581L15.3161 7.34193M15.3161 7.34193C15.8087 8.32508 16.8255 9 18 9C19.6569 9 21 7.65685 21 6C21 4.34315 19.6569 3 18 3C16.3431 3 15 4.34315 15 6C15 6.48237 15.1138 6.93815 15.3161 7.34193ZM15.3161 16.6581C15.1138 17.0619 15 17.5176 15 18C15 19.6569 16.3431 21 18 21C19.6569 21 21 19.6569 21 18C21 16.3431 19.6569 15 18 15C16.8255 15 15.8087 15.6749 15.3161 16.6581Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+            </svg>Share This Page</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row g-0" style="margin-top: 1.5rem;">
+      <div class="col-md-3 col-lg-3 actor-cont">
+        <div class="d-flex justify-content-center align-items-center mx-auto myprofile"><img class="img-fluid" src="assets/img/xigncode_cat_no_background-cropped-x2.png" alt="bs4_team_01" style="border-radius: 5px;"></div>
+        <section>
+          <div class="timeline"></div>
+          <ul class="timeline">
+            <li class="bg-white shadow timeline-item rounded ms-3 p-4">
+              <div class="timeline-arrow"></div>
+              <div>
+                <h2 class="h5 mb-0">Title of section 1</h2><span class="text-secondary small"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" class="bi bi-clock me-1">
+                                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm8-7A8 8 0 1 1 0 8a8 8 0 0 1 16 0z"></path>
+                                        <path fill-rule="evenodd" d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"></path>
+                                    </svg>21 March, 2019</span>
+                <p class="text-small mt-2 fw-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
+              </div>
+            </li>
+            <li class="bg-white shadow timeline-item rounded ms-3 p-4">
+              <div class="timeline-arrow"></div>
+              <div>
+                <h2 class="h5 mb-0">Title of section 2</h2><span class="text-secondary small"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" class="bi bi-clock me-1">
+                                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm8-7A8 8 0 1 1 0 8a8 8 0 0 1 16 0z"></path>
+                                        <path fill-rule="evenodd" d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"></path>
+                                    </svg>5 April, 2019</span>
+                <p class="text-small mt-2 fw-light">Libero expedita explicabo eius fugiat quia aspernatur autem laudantium error architecto recusandae natus sapiente sit nam eaque, consectetur porro molestiae ipsam! Deleniti.</p>
+              </div>
+            </li>
+            <li class="bg-white shadow timeline-item rounded ms-3 p-4">
+              <div class="timeline-arrow"></div>
+              <div>
+                <h2 class="h5 mb-0">Title of section 3</h2><span class="text-secondary small"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" class="bi bi-clock me-1">
+                                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm8-7A8 8 0 1 1 0 8a8 8 0 0 1 16 0z"></path>
+                                        <path fill-rule="evenodd" d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"></path>
+                                    </svg>18 August, 2019</span>
+                <p class="text-small mt-2 fw-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
+              </div>
+            </li>
+            <li class="bg-white shadow timeline-item rounded ms-3 p-4">
+              <div class="timeline-arrow"></div>
+              <div>
+                <h2 class="h5 mb-0">Title of section 4</h2><span class="text-secondary small"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" class="bi bi-clock me-1">
+                                        <path fill-rule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm8-7A8 8 0 1 1 0 8a8 8 0 0 1 16 0z"></path>
+                                        <path fill-rule="evenodd" d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"></path>
+                                    </svg>10 October, 2019</span>
+                <p class="text-small mt-2 fw-light">Voluptatibus temporibus esse illum eum aspernatur, fugiat suscipit natus! Eum corporis illum nihil officiis tempore. Excepturi illo natus libero sit doloremque, laborum molestias rerum pariatur quam ipsam necessitatibus incidunt, explicabo.</p>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </div>
+      <div class="col-md-9 col-lg-7">
+        <section>
+          <div class="container">
+            <p>From frontend frameworks to backend languages and DevOps tools, I try to stay up to date with the current state of the web development ecosystem.<br></p>
+            <h1 style="text-align: center;">My Stack</h1><img class="img-fluid" src="assets/img/mystacks.jpg">
+          </div>
+        </section>
+        <section>
+          <div class="container">
+            <h1 style="text-align: center;"><strong>Portfolio</strong><br></h1>
+            <p>Here are some of my featured projects that i worked on in recent years.<br></p>
 
+            <div class="container mx-auto px-4 w-full md:w-3/4 lg:w-3/5 xl:w-1/2 my-20">
+              <h2 class="text-4xl">All Posts</h2>
+              <div v-if="$apollo.loading"></div>
+
+              <div v-else>
+                <ProjectListItem v-for="wp_post in wp_posts" :key="wp_post.ID" :wp_post="wp_post" class="mt-10"></ProjectListItem>
+              </div>
+            </div>
+          </div>
+
+        </section>
+        <section>
+          <div class="container">
+            <h1 style="text-align: center;"><strong>Get in touch&nbsp;👋</strong><br></h1>
+            <p>I'm happy to talk business or simply give you some free advice. If both sides see value in working together, we’ll move forward. If not, that's okay too. Worst case we both had a nice chat and received some free feedback.<br></p>
+          </div>
+        </section><p data-v-6b64169c="" class="teaser narrow text-heading"><a data-v-6b64169c="" href="mailto:knightybird@gmail.com">Send me an email at knightybird@gmail.com</a></p>
+      </div>
+      <div class="col-md-12 col-lg-2"></div>
+    </div>
   </div>
 
 </template>
 
 <script>
 import NavbarDefault from "../../../components/core/navbars/NavbarDefault";
+import ProjectListItem from "../portfolio/ProjectListItem";
+import gql from "graphql-tag";
 
 export default {
   name: "About",
   components: {
+    ProjectListItem,
     NavbarDefault
   },
-}
+  apollo: {
+    wp_posts: gql`
+    {
+      wp_posts(where: {AND: [
+        { column: POST_TYPE, operator: EQ, value: "jetpack-portfolio" }
+        { column: POST_STATUS, operator: EQ, value: "publish" }
+      ]
+      }) {
+        ID
+        post_title
+        post_excerpt
+        post_content
+        post_date
+        post_author
+      }
+    }
+    `
+  }
+};
 </script>
 
 <style scoped>
